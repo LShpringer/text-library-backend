e13ac469457ebf7a29fe206d190e9eae8ca778db
LShpringer <Editor@lshpringer.ru>
2026-03-27 18:55:26 +0700

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("postgresql://postgres:helenahunter2505!@db.pmjsdqxddcisdqjpjcvo.supabase.co:5432/postgres", "sqlite:///./library.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./library.db")
 
 # Для Postgres connect_args пустые; для sqlite нужен check_same_thread
 connect_args = {}
