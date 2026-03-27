57aee1b3d4dac2a8492c108ec4d756d4e8422030
LShpringer <Editor@lshpringer.ru>
2026-03-27 17:14:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from fastapi import Depends, HTTPException, Security
+from fastapi.security.api_key import APIKeyHeader
+from starlette import status
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
@@ -6,6 +9,20 @@ from routes.texts import router as texts_router
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Библиотека текстов")
+
+API_KEY = "super-secret-key"  # придумай свой
+API_KEY_NAME = "X-Admin-Key"
+
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
+
+def verify_api_key(api_key: str = Security(api_key_header)):
+    if api_key != API_KEY:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or missing API Key",
+        )
+    return api_key
+
 
 app.add_middleware(
     CORSMiddleware,
